4bed9b5102404e113d89b6b98a66832bcc51e0fb
xiaocong <xiaocong@qq.com>
2019-11-16 20:27:48 +0800

    关联mysql数据库，设计文章模型类

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+# 定义图书模型类
+class Article(models.Model):
+    # 文章的标题
+    title = models.CharField(max_length=40, null=False, verbose_name='标题')
+
+    # 文章的摘要
+    brief_content = models.TextField(verbose_name='摘要')
+
+    # 文章的主要内容
+    content = models.TextField(verbose_name='内容')
+
+    # 文章的发布日期
+    publish_date = models.DateTimeField(auto_now=True, verbose_name='发布日期')
+
+    # 逻辑删除
+    is_delete = models.BooleanField(default=False, verbose_name='逻辑删除')
+
+    class Meta:
+        db_table = 'tb_article'
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'myblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'blog/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,13 +74,23 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',  # 数据库主机
+        'PORT': 3306,  # 数据库端口
+        'USER': 'root',  # 数据库用户名
+        'PASSWORD': 'root',  # 数据库用户密码
+        'NAME': 'blog_article'  # 数据库名字
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
